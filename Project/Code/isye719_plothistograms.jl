ae93bb945210ca370f3db40e66c5b6fda54962ac
Ranjeet Kumar <rkumar32@wisc.edu>
2016-12-18 12:27:08 -0600

    added adding histograms code

--- Project/Code/isye719_plothistograms.jl
+++ Project/Code/isye719_plothistograms.jl
@@ -1,5 +1,9 @@
 using PyPlot
 
+obj_st_fp_all = -obj_st_fp_all;
+obj_dt_fp_all = -obj_dt_fp_all;
+obj_pi_fp_all = -obj_pi_fp_all;
+obj_st_fp_socfix_all = -obj_st_fp_socfix_all;
 # PLOTTING HISTOGRAM OF ST COSTS
 PyPlot.rc("ytick",labelsize=14)
 PyPlot.rc("xtick",labelsize=14)
@@ -9,20 +13,26 @@ plt[:get_current_fig_manager]()[:full_screen_toggle]()
 sub1 = subplot(4,1,1)
 grid()
 hold(true)
-hist1 = plt[:hist](committedTotalCost_ST_VECTOR,nbins,weights=ones(length(committedTotalCost_ST_VECTOR))/length(committedTotalCost_ST_VECTOR),color="blue",label="Stochastic");
+hist1 = plt[:hist](obj_dt_fp_all,nbins*2,weights=ones(length(obj_dt_fp_all))/length(obj_dt_fp_all),color="indigo",label="Mean-value problem");
 # PLOTTING HISTOGRAM OF DET COSTS
-sub2 = subplot(3,1,2)
+sub2 = subplot(4,1,2)
 grid()
 hold(true)
-hist2 = plt[:hist](committedTotalCost_DET_VECTOR,nbins,weights=ones(length(committedTotalCost_DET_VECTOR))/length(committedTotalCost_DET_VECTOR),color="red",label="Deterministic");
-sub3 = subplot(3,1,3)
+hist2 = plt[:hist](obj_st_fp_socfix_all,nbins,weights=ones(length(obj_st_fp_socfix_all))/length(obj_st_fp_socfix_all),color="red",label="Two Stage: Restriction");
+# PLOTTING HISTOGRAM OF PI COSTS
+sub3 = subplot(4,1,3)
 grid()
 hold(true)
-hist3 = plt[:hist](committedTotalCost_PI_VECTOR,nbins,weights=ones(length(committedTotalCost_PI_VECTOR))/length(committedTotalCost_PI_VECTOR),color="green",label="Perfect Information");
+hist3 = plt[:hist](obj_st_fp_all,nbins,weights=ones(length(obj_st_fp_all))/length(obj_st_fp_all),color="blue",label="Stochastic");
+# PLOTTING HISTOGRAM OF PI COSTS
+sub4 = subplot(4,1,4)
+grid()
+hold(true)
+hist4 = plt[:hist](obj_pi_fp_all,nbins,weights=ones(length(obj_pi_fp_all))/length(obj_pi_fp_all),color="green",label="Perfect Information");
 # Setting xlims and ylims based on default lims of the two subplots
-xlims = [collect(sub1[:get_xlim]()) collect(sub2[:get_xlim]()) collect(sub3[:get_xlim]())];
+xlims = [collect(sub1[:get_xlim]()) collect(sub2[:get_xlim]()) collect(sub3[:get_xlim]()) collect(sub4[:get_xlim]())];
 xlims = [minimum(xlims),maximum(xlims)];
-ylims = [collect(sub1[:get_ylim]()) collect(sub2[:get_ylim]()) collect(sub3[:get_ylim]())];
+ylims = [collect(sub1[:get_ylim]()) collect(sub2[:get_ylim]()) collect(sub3[:get_ylim]()) collect(sub4[:get_ylim]())];
 ylims = [minimum(ylims),maximum(ylims)];
 sub1[:set_xlim](xlims)
 sub1[:set_ylim](ylims)
@@ -30,18 +40,24 @@ sub2[:set_xlim](xlims)
 sub2[:set_ylim](ylims)
 sub3[:set_xlim](xlims)
 sub3[:set_ylim](ylims)
+sub4[:set_xlim](xlims)
+sub4[:set_ylim](ylims)
 subplot(sub1)
-plot(Expected_Total_Cost_ST*ones(2),ylims,LineWidth=3,color="black",LineStyle="--",label="Mean Cost") # Plot vertical line at expected value
+plot(mean(obj_dt_fp_all)*ones(2),ylims,LineWidth=3,color="black",LineStyle="--",label="Expected Revenue") # Plot vertical line at expected value
 ylabel("Probability",size = 14)
 legend(loc="upper left",fancybox="True", shadow="True", fontsize = 14)
 subplot(sub2)
-plot(Expected_Total_Cost_DET*ones(2),ylims,LineWidth=3,color="black",LineStyle="--",label="Mean Cost") # Plot vertical line at expected value
+plot(mean(obj_st_fp_socfix_all)*ones(2),ylims,LineWidth=3,color="black",LineStyle="--",label="Expected Revenue") # Plot vertical line at expected value
 ylabel("Probability",size = 14)
 legend(loc="upper left",fancybox="True", shadow="True", fontsize = 14)
 subplot(sub3)
-plot(Expected_Total_Cost_PI*ones(2),ylims,LineWidth=3,color="black",LineStyle="--",label="Mean Cost") # Plot vertical line at expected value
-ylabel("Probability",size = 14)
-xlabel("Total Cost (\$/month)",size = 24)
+plot(mean(obj_st_fp_all)*ones(2),ylims,LineWidth=3,color="black",LineStyle="--",label="Expected Revenue") # Plot vertical line at expected value
 legend(loc="upper left",fancybox="True", shadow="True", fontsize = 14)
-savefig(string(FiguresDirectory,"/histogram_costs_$(hor)day$(scaledf[scale]).pdf"))
+ylabel("Probability",size = 14)
+subplot(sub4)
+plot(mean(obj_pi_fp_all)*ones(2),ylims,LineWidth=3,color="black",LineStyle="--",label="Expected Revenue") # Plot vertical line at expected value
+ylabel("Probability",size = 14)
+xlabel("Revenue (\$)",size = 24)
+legend(loc="upper left",fancybox="True", shadow="True", fontsize = 14)
+savefig(string(figuredirectory,"/histogram_costs.pdf"))
 close("all")
